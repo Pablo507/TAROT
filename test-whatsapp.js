63ecf627489c23749ed8369c11d24c27979a6d0b
Pablo507 <45787130+Pablo507@users.noreply.github.com>
2026-08-02 08:01:04 -0300

    Change WhatsApp template language code to es_UY

--- test-whatsapp.js
+++ test-whatsapp.js
@@ -20,7 +20,7 @@ async function sendTestMessage() {
       type: "template",
       template: {
         name: "carta_diaria",
-        language: { code: "es_AR" },
+        language: { code: "es_UY" },
         components: [
           {
             type: "body",
